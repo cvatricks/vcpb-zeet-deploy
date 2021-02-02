9454cca785d4905580736ec6b309f459a8275c37
cvatricks <68840422+cvatricks@users.noreply.github.com>
2021-02-02 19:38:52 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,15 +5,15 @@ import ast
 from os import environ
 from pyrogram import filters
 
-API_ID = environ.get("API_ID")
-API_HASH = environ.get("API_HASH")
-TOKEN = environ.get("TOKEN")
-SUDO_USERS = environ.get("SUDO_USERS")
-GROUP = environ.get("GROUP")
-MONGO_DB_URI = environ.get("MONGO_DB_URI")
-USERS_MUST_JOIN = environ.get("USERS_MUST_JOIN", "False")
-LANG = environ.get("LANG", "en")
-DUR_LIMIT = environ.get("DUR_LIMIT", 10)
+API_ID = "1566635"
+API_HASH = "407389a923d7a47a31b0deaeb22d24ae"
+TOKEN = "1416304053:AAFkAx7Fr4A0V3ZkySAbzGPEwkh5Hj-WMgc"
+SUDO_USERS = "695291232"
+GROUP = "-1001440655092"
+MONGO_DB_URI = ""
+USERS_MUST_JOIN = "true"
+LANG = "en"
+DUR_LIMIT = "10"
 
 API_ID = int(API_ID)
 SUDO_USERS = list(map(int, SUDO_USERS.split()))
